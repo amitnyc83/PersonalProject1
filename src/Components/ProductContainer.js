17808d948e72f5ca37a1b2f363ddf0f9c2288dfa
amitnyc83 <amit_nyc@live.com>
2019-09-27 11:37:58 -0400

    styling individual product page

--- src/Components/ProductContainer.js
+++ src/Components/ProductContainer.js
@@ -33,16 +33,23 @@ class ProductContainer extends Component {
   }
 
   render() {
-    // console.log(this.props.product)
     return (
-      <div>
+      <React.Fragment>
         {this.state.clickedProduct === null ? this.mapProduct() :
-          <div>
-            <div>Selected product</div>
+          <React.Fragment>
+            <div id="product-info-main-container">
+              <img id="product-info-image" src={this.state.clickedProduct.image} alt="sorry"/>
+              <div>
+                <div id="product-info-title">{this.state.clickedProduct.title}</div>
+                <div id="product-info-name">{this.state.clickedProduct.name}</div>
+                <div id="product-info-price"> ${this.state.clickedProduct.price}</div>
+                <div id="product-info-description"> {this.state.clickedProduct.description}</div>
+              </div>
+            </div>
             <button onClick={this.clearState}>All Products</button>
-          </div>
+          </React.Fragment>
         }
-      </div>
+      </React.Fragment>
     )
   }
 }
