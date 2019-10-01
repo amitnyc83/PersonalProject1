7597fde84fee66d0bc0f6d0c9611f1f0f12ae317
amitnyc83 <amit_nyc@live.com>
2019-09-30 22:49:50 -0400

    adds feature to update product from sellers acc

--- src/Components/SellerProdContainer.js
+++ src/Components/SellerProdContainer.js
@@ -7,6 +7,49 @@ import { sellerDeleteProduct } from '../Store/Actions/product_action'
 
 class SellerProdContainer extends Component {
 
+  state = {
+    name: null,
+    price: null,
+    description: null,
+    brand: null,
+    cost: null,
+    quantity: null
+  }
+
+  handleChange = (event, productInfo) => {
+    this.setState({
+      name: productInfo.name,
+      price: productInfo.price,
+      description: productInfo.description,
+      brand: productInfo.brand,
+      cost: productInfo.cost,
+      quantity: productInfo.quantity
+    })
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleUpdateSubmit = (e, product) => {
+    e.preventDefault()
+    fetch(`http://localhost:3001/products/${product.id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-type": "application/json",
+      Accepts: "application/json"
+    },
+    body: JSON.stringify({
+      name: this.state.name,
+      brand: this.state.brand,
+      price: this.state.price,
+      description: this.state.description,
+      cost: this.state.cost,
+      quantity: this.state.quantity
+    })
+  }).then(resp => resp.json())
+    .then(response => console.log(response))
+  }
+
 
   deleteOwnProduct = (e, product) => {
     e.preventDefault()
@@ -23,25 +66,34 @@ class SellerProdContainer extends Component {
       return product["seller_id"] === this.props.currentUser["user_id"]
     }).map(productInfo => {
       return <div class="main-sellerpage-container">
-        <label class="label-seller-product-brand-info">Brand:</label>
-        <span class="seller-product-info-brand"> {productInfo.brand} </span>
-        <br></br>
-        <label class="label-seller-product-info">Name:</label>
-        <span class="seller-product-info"> {productInfo.name} </span>
-        <br></br>
-        <label class="label-seller-product-info">Price:</label>
-        <span class="seller-product-info"> ${productInfo.price} </span>
-        <br></br>
-        <label class="label-seller-product-info">Description:</label>
-        <span class="seller-product-description"> {productInfo.description} </span>
-        <br></br>
-        <label class="label-seller-product-brand-info">Cost:</label>
-        <span class="label-seller-product-brand"> ${productInfo.cost} </span>
-        <br></br>
-        <label class="label-seller-product-brand-info">Profit Margin/item</label>
-        <span class='label-seller-product-brand'>{((productInfo.cost / productInfo.price) * 100).toFixed(2)}%</span>
-        <img class="seller-product-info-image" src={productInfo.image} />
-         <button class="seller-deletebutton" onClick={(e) => this.deleteOwnProduct(e, productInfo)}>Delete</button>
+        <form  className="seller-update-form" onSubmit={(e) => this.handleUpdateSubmit(e, productInfo)}>
+          <label class="label-seller-product-info">Name:</label>
+          <input class="seller-product-info" placeholder={productInfo.name}  onChange={(event) => this.handleChange(event, productInfo)} name="name"/>
+          <br></br>
+          <label class="label-seller-product-info">Price:$</label>
+          <input class="seller-product-info-price" placeholder={productInfo.price}  onChange={(event) => this.handleChange(event, productInfo)} name="price" />
+          <br></br>
+          <label class="label-seller-product-brand-info">Brand:</label>
+          <input class="seller-product-info-brand" placeholder={productInfo.brand} onChange={(event) => this.handleChange(event, productInfo)} name="brand"/>
+          <br></br>
+          <label class="label-seller-product-info">Description:</label>
+          <textarea class="seller-product-description" placeholder={productInfo.description} onChange={(event) => this.handleChange(event, productInfo)} name="description"/>
+          <br></br>
+          <label class="label-seller-product-brand-info">Cost:$</label>
+          <input class="seller-product-info-price" placeholder={productInfo.cost} onChange={(event) => this.handleChange(event, productInfo)} name="cost"/>
+          <br></br>
+          <label class="label-seller-product-brand-info">Profit Margin per Item:</label>
+          <span class="seller-product-info-brand">{((( productInfo.price - productInfo.cost )/ productInfo.price) * 100).toFixed(2)}%</span>
+          <br></br>
+          <label class="label-seller-product-brand-info">Current Quantity:</label>
+          <input class="seller-product-info-price" placeholder={productInfo.quantity} onChange={(event) => this.handleChange(event, productInfo)} name="quantity"/>
+          <br></br>
+          <img class="seller-product-info-image" src={productInfo.image} />
+          <button class="seller-editbutton" >Update</button>
+        </form>
+        <div className="deletebutton-div">
+          <button class="seller-deletebutton" onClick={(e) => this.deleteOwnProduct(e, productInfo)}>Delete</button>
+        </div>
       </div>
     })
   }
