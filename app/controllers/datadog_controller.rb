54b56667e026ca75e8b5d8fc128f935c2da30761
amitnyc83 <amit_nyc@live.com>
2019-12-10 14:08:28 -0500

    update config.yml file

--- app/controllers/datadog_controller.rb
+++ app/controllers/datadog_controller.rb
@@ -1,10 +1,11 @@
-require 'datadog/statsd'
+require 'rubygems'
+require 'dogapi'
 
-statsd = Datadog::Statsd.new('localhost', 8125)
+api_key = "816226ca9d3053634e34827fff3c8915"
+application_key = "<YOUR_DD_APP_KEY>"
 
-while true do
-    statsd.increment('example_metric.increment', tags: ['environment:dev'])
-    statsd.decrement('example_metric.decrement', tags: ['environment:dev'])
-    statsd.count('example_metric.count', 2, tags: ['environment:dev'])
-    sleep 10
-end
+# Submitting events does not require the application key.
+dog = Dogapi::Client.new(api_key)
+
+# Send a new event.
+dog.emit_event(Dogapi::Event.new('this is a test message', :msg_title => 'Test Message'))
